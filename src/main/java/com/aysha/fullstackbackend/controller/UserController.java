a36919fc635592dc6d3ec9da7e559a7ee5ad734e
Aysha-Ramzy <aysharamzy1998@gmail.com>
2023-08-05 13:38:52 +0530

    Created database to Add User

--- src/main/java/com/aysha/fullstackbackend/controller/UserController.java
+++ src/main/java/com/aysha/fullstackbackend/controller/UserController.java
@@ -3,25 +3,29 @@ package com.aysha.fullstackbackend.controller;
 import java.util.List;
 import com.aysha.fullstackbackend.model.User;
 import com.aysha.fullstackbackend.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
-    @Autowired
 
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @PostMapping("/user")
     User newUser(@RequestBody User newUser){
-return userRepository.save(newUser);//post user to frontend
+return userRepository.save(newUser);//post user
     }
     @GetMapping("/users")
     List<User> getAllUsers(){
 return userRepository.findAll(); //get to frontend
     }
 
+    @GetMapping("/user/{id}")
+    void getUserById(@PathVariable Long id){
+        //return
+    }
 }
